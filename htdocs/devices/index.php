8fbbbdb9f3e6093980c5ddeba6943e347a73cb56
Sergey Fedotov <fsa@tavda.info>
2018-06-11 08:41:38 +0700

    Список устройств в виде таблицы (черновой вариант без заголовков HTML)

--- htdocs/devices/index.php
+++ htdocs/devices/index.php
@@ -5,22 +5,56 @@ $mem=new Shm();
 $yeelight=$mem->getVar(1);
 $xiaomi=$mem->getVar(2);
 ?>
+<style>
+    table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+    
+    th,td {
+        border: solid 1px black;
+        padding: 0.3rem;
+    }
+</style>
 <h1>Xiaomi</h1>
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Наименование</th>
+        <th>Информация</th>
+        <th>Был активен</th>
+    </tr>
 <?php
 foreach ($xiaomi as $dev) {
 ?>
-<p><?=$dev->getDeviceName()?></p>
-<p>sid: <?=$dev->getDeviceId()?>. Last Update: <?=$dev->getLastUpdate()?></p>
-<hr>
+    <tr>
+        <td><?=$dev->getDeviceId()?></td>
+        <td><?=$dev->getDeviceName()?></td>
+        <td></td>
+        <td><?=$dev->getLastUpdate()?></td>
+    </tr>
 <?php
 }
 ?>
+</table>
 <h1>Yeelight</h1>
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Наименование</th>
+        <th>Информация</th>
+        <th>Был активен</th>
+    </tr>
 <?php
 foreach ($yeelight as $dev) {
 ?>
-<p><?=$dev->getDeviceName()?></p>
-<p>sid: <?=$dev->getDeviceId()?>. Last Update: <?=$dev->getLastUpdate()?></p>
-<hr>
+    <tr>
+        <td><?=$dev->getDeviceId()?></td>
+        <td><?=$dev->getDeviceName()?></td>
+        <td></td>
+        <td><?=$dev->getLastUpdate()?></td>
+    </tr>
 <?php
 }
+?>
+</table>
