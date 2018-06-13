e37560eda07fc60d49252a2c6264c85e23275e35
Sergey Fedotov <fsa@tavda.info>
2018-06-14 04:36:45 +0700

    Реорганизация шаблона HTML. Стили вынесены в отдельный файл. Сам файл формируется автоматом из scss через gulp.

--- classes/templates/html.php
+++ classes/templates/html.php
@@ -6,20 +6,15 @@ class HTML {
 
     public function header() {
 ?>
+<!DOCTYPE html>
 <html>
 <head>
+<meta charset="utf-8">
 <title><?=$this->title?></title>
-<style>
-    table {
-        width: 100%;
-        border-collapse: collapse;
-    }
-    
-    th,td {
-        border: solid 1px black;
-        padding: 0.3rem;
-    }
-</style>
+<meta name="viewport" content="width=device-width">
+<meta name="theme-color" content="gray">
+<link rel="stylesheet" href="/styles.css">
+<?=$this->header?>
 </head>
 <body>
 <?php
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglifyjs'),
-    cssnano = require('gulp-cssnano'),
-    rename = require('gulp-rename'),
-    del = require('del'),
-    browserSync = require('browser-sync');
+        sass = require('gulp-sass'),
+        autoprefixer = require('gulp-autoprefixer'),
+        concat = require('gulp-concat'),
+        uglify = require('gulp-uglifyjs'),
+        cssnano = require('gulp-cssnano'),
+        rename = require('gulp-rename'),
+        del = require('del'),
+        browserSync = require('browser-sync');
 
 gulp.task('default', function () {
     console.log('Нет действия по умолчанию.');
@@ -25,21 +25,26 @@ gulp.task('browser-sync', ['scss'], function () {
 
 gulp.task('scss', function () {
     return gulp.src('src/scss/**/*.+(scss|sass)')
-      .pipe(sass({outputStyle: 'expanded'}))
+            .pipe(sass(
+                    {
+                        outputStyle: 'expanded',
+                        includePaths: ["node_modules/bootstrap/scss/"]
+                    }
+            ))
 //    .pipe(concat('styles.css'))
-      .pipe(autoprefixer(['last 15 versions', '>1%', 'ie 8']))
+            .pipe(autoprefixer(['last 15 versions', '>1%', 'ie 8']))
 //    .pipe(cssnano())
-      .pipe(gulp.dest('htdocs'))
-      .pipe(browserSync.reload({stream: true}));
+            .pipe(gulp.dest('htdocs'))
+            .pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('jslibs', ['jquery','leaflet','highcharts'], function () {
+gulp.task('jslibs', ['jquery', 'leaflet', 'highcharts'], function () {
     console.log('Копирование мелких библиотек js');
     return gulp.src([
         'node_modules/html5shiv/dist/html5shiv.min.js',
     ])
-      .pipe(gulp.dest('htdocs/libs'))
-      .pipe(browserSync.reload({stream: true}));
+            .pipe(gulp.dest('htdocs/libs'))
+            .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('leaflet', function () {
@@ -49,8 +54,8 @@ gulp.task('leaflet', function () {
         'node_modules/leaflet/dist/leaflet.css',
         'node_modules/leaflet/dist/**/*.png'
     ])
-        .pipe(gulp.dest('htdocs/libs/leaflet'));
-;
+            .pipe(gulp.dest('htdocs/libs/leaflet'));
+    ;
 });
 
 gulp.task('highcharts', function () {
@@ -59,13 +64,13 @@ gulp.task('highcharts', function () {
         'node_modules/highcharts/highcharts.js',
         'node_modules/highcharts/modules/exporting.js'
     ])
-         .pipe(gulp.dest('htdocs/libs/highcharts'));    
+            .pipe(gulp.dest('htdocs/libs/highcharts'));
 });
 
 gulp.task('jquery', function () {
     console.log('Копирование jquery');
     return gulp.src([
-        'node_modules/jquery/dist/jquery.min.js',    
+        'node_modules/jquery/dist/jquery.min.js',
     ])
-        .pipe(gulp.dest('htdocs/libs/jquery'));    
+            .pipe(gulp.dest('htdocs/libs/jquery'));
 });
