e0899179ce85d28b3e02679d975750943896967e
Sergey Fedotov <fsa@tavda.info>
2018-06-18 20:55:27 +0700

    Добавлены настройки для демона воспроизведения речи. Настраивается звук перед первым сообщением и команда для его воспроизведения.

--- classes/smarthome/ttsdaemon.php
+++ classes/smarthome/ttsdaemon.php
@@ -4,18 +4,23 @@ namespace SmartHome;
 
 class TtsDaemon implements Daemon {
 
-    const COMMAND='mpg123 -q';
+    const PLAY_SOUND_CMD='mpg123 -q %s';
     const PRE_SOUND='notification.mp3';
     const PRE_SOUND_PERIOD=5;
 
     private $tts_provider;
     private $queue;
     private $last_message_time;
+    private $pre_sound;
+    private $play_sound_cmd;
 
     public function __construct($precess_url) {
         $this->queue=new TtsQueue;
         $tts=file_get_contents(__DIR__.'/../../config/tts.conf');
         $this->tts_provider=unserialize($tts);
+        $settings=\Settings::get('tts');
+        $this->pre_sound=isset($settings->pre_sound)?$settings->pre_sound:self::PRE_SOUND;
+        $this->play_sound_cmd=isset($settings->play_sound_cmd)?$settings->play_sound_cmd:self::PLAY_SOUND_CMD;
     }
 
     public function getName() {
@@ -38,13 +43,13 @@ class TtsDaemon implements Daemon {
     private function playVoice($text) {
         $filename=$this->tts_provider->getVoiceFile($text);
         if(time()-$this->last_message_time>self::PRE_SOUND_PERIOD) {
-            $this->playMp3(__DIR__.'/../../sound/'.self::PRE_SOUND);
+            $this->playMp3(__DIR__.'/../../sound/'.$this->pre_sound);
         }
         $this->playMp3($filename);
         $this->last_message_time=time();        
     }
 
     private function playMp3(string $filename) {
-        system(self::COMMAND.' '.$filename);
+        system(sprintf($this->play_sound_cmd,$filename));
     }
 }
