288b834ad45fc318464eb1478d151a1ee80a42e5
Sergey Fedotov <fsa@tavda.info>
2018-07-04 03:36:39 +0700

    Добавлена папка пользовательских скриптов custom, создан подключаемый файл с полезными фукнциями и переменными. Добавлены системные переменные хранящиеся в базе данных. Добавлены ночной режим и режим охраны.

---
+++ custom/minutely.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once 'lib.php';
+if(file_exists('minutely_custom.php')) {
+    include 'minutely_custom.php';
+}
---
+++ custom/minutely.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd `dirname $0`
+timeout 30 /usr/bin/php -q -f minutely.php
