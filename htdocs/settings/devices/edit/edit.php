e98ab25181b8f6036689e32e859377f021ab5278
Sergey Fedotov <fsa@tavda.info>
2018-07-06 14:25:52 +0700

    При регистрации нового устройства учитываются данные инициализации.

--- htdocs/settings/devices/edit/edit.php
+++ htdocs/settings/devices/edit/edit.php
@@ -5,17 +5,20 @@ if (!isset($action)) {
 }
 $device=new \SmartHome\Entity\Device;
 $device->id=filter_input(INPUT_POST,'id',FILTER_VALIDATE_INT);
+$device->unique_name=filter_input(INPUT_POST,'unique_name');
 $device->module_id=filter_input(INPUT_POST,'module_id',FILTER_VALIDATE_INT);
 $device->uid=filter_input(INPUT_POST,'uid');
-$device->classname=filter_input(INPUT_POST,'classname');
-$device->unique_name=filter_input(INPUT_POST,'unique_name');
 $device->name=filter_input(INPUT_POST,'name');
+$device->classname=filter_input(INPUT_POST,'classname');
+$init=filter_input(INPUT_POST,'init',FILTER_DEFAULT,FILTER_REQUIRE_ARRAY);
+if(is_array($init)) {
+    $device->init_data=json_encode($init);
+}
 $device->place_id=filter_input(INPUT_POST,'place_id',FILTER_VALIDATE_INT);
 if(!$device->place_id) {
     $device->place_id=null;
 }
 $device->disabled=0;
-#TODO получить init_data
 $devices=new \SmartHome\Devices;
 $devices->setDevice($device);
 if($device->id) {
