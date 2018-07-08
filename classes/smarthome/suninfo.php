16c48d86ef93526e8077966b808cabed99f7c0f9
Sergey Fedotov <fsa@tavda.info>
2018-07-09 00:29:56 +0700

    Класс для определения времени информации о солнце и выдачи текстовых сообщений о событиях восхода, заката и солнца в зените.

---
+++ classes/smarthome/suninfo.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace SmartHome;
+
+class SunInfo {
+
+    private $sun_info;
+
+    public function __construct() {
+        $home=\Settings::get('home');
+        $this->sun_info=date_sun_info(time(),$home->lat,$home->lon);
+    }
+
+    public function getText(string $time): string {
+        foreach ($this->sun_info as $name=> $value) {
+            if ($value and $time==date('H:i',$value)) {
+                switch ($name) {
+                    case "sunrise":
+                        return "Восход солнца";
+                    case "sunset":
+                        return "Закат солнца";
+                    case "transit":
+                        return "Солнце в зените";
+                }
+            }
+        }
+        return false;
+    }
+    
+    public function getSunset() {
+        return $this->sun_info['sunset'];
+    }
+
+    public function getSunrise() {
+        return $this->sun_info['sunrise'];
+    }
+}
