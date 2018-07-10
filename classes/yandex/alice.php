71aae2ba1f95a4f49ce7989259ecb5ba0bf14870
Sergey Fedotov <fsa@tavda.info>
2018-07-10 17:36:12 +0700

    Навык-заглушка для Яндекс Алисы.

---
+++ classes/yandex/alice.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Yandex;
+
+class Alice {
+
+    private $meta;
+    private $request;
+    private $session;
+    private $version;
+    
+    private $result;
+
+    public function __construct($request) {
+        $data=json_decode($request,true);
+        $this->meta=$data['meta'];
+        $this->request=$data['request'];
+        $this->session=$data['session'];
+        $this->version=$data['version'];
+        $this->result['session']=['session_id'=>$this->session['session_id'],'message_id'=>$this->session['message_id'],'user_id'=>$this->session['user_id']];
+        $this->resetResponce();
+        $this->result['version']='1.0';
+    }
+    
+    public function checkSkillId($skill_id) {
+        if(!is_null($skill_id)) {
+            if($this->session['skill_id']!=$skill_id) {
+                throw new \AppException('Я не обрабатываю запросы для этого навыка!');            
+            }
+        }        
+    }
+    
+    public function isNewDialog() {
+        return $this->session['new']==1;
+    }
+    
+    public function getRequest() {
+        return $this->request;
+    }
+    
+    public function getSession() {
+        return $this->session;
+    }
+    
+    public function setText($text,$tts=null) {
+        $search=['&nbsp;','&deg;'];
+        $replace=[' ','°'];
+        $this->result['response']['text']=str_replace($search,$replace,$text);
+        if(!is_null($tts)) {
+            $this->result['response']['tts']=$tts;
+        }
+    }
+    
+    public function setEndSession(bool $value=true) {
+        $this->result['response']['end_session']=$value;
+    }
+    
+    public function resetResponce() {
+        $this->result['response']=['end_session'=>false];
+    }
+    
+    public function getResponse() {
+        header('Content-Type: application/json');
+        echo json_encode($this->result,JSON_UNESCAPED_UNICODE);
+    }
+}
---
+++ htdocs/alice-webhook/index.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once '../common.php';
+$alice=new Yandex\Alice(file_get_contents('php://input'));
+$result_tts=false;
+try {
+    #$alice->checkSkillId('skill_id');
+    $request=$alice->getRequest();
+    $command=mb_strtolower($request['command']);
+    if ($alice->isNewDialog()) {
+        $result="Умный дом приветствует вас";
+        #$result_tts="Умный дом приветствует вас";
+    } else {
+        $result='Я пока ничего не умею.';
+        $result_tts="Я пока ничего не умею.";
+    }
+    if (!$result_tts) {
+        $alice->setText($result);
+    } else {
+        $alice->setText($result,$result_tts);
+    }
+} catch (AppException $ex) {
+    $alice->setText($ex->getMessage());
+} catch (Exception $ex) {
+    $alice->setText('Сервер сообщает мне, что произошла какая-то ошибка.');
+}
+$alice->getResponse();
