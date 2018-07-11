4ec2226a4467b3d68516742f98c3c67cae2592a8
Sergey Fedotov <fsa@tavda.info>
2018-07-12 02:34:46 +0700

    Преобразование сущностей HTML в символы для JS.

--- htdocs/charts/show_unit.php
+++ htdocs/charts/show_unit.php
@@ -9,7 +9,7 @@ if(!$meter) {
 }
 $series=[];
 foreach (SmartHome\Meters::getMetersByUnitId($unit) as $serie) {
-    $series[]="{name: '$serie->name', params: {place: $serie->place_id, unit: $unit}}";
+    $series[]="{name: '".html_entity_decode($serie->name)."', params: {place: $serie->place_id, unit: $unit}}";
 }
 if(sizeof($series)==0) {
     throw new AppException('Не найдены активные датчики для измерения: '.$meter->name);
@@ -22,8 +22,8 @@ HTML::showPageHeader($meter->name);
     var date = new Date();
     date.setDate(date.getDate() - 2);
     var series = [<?=join(',',$series)?>];
-    var title = '<?=$meter->name?>';
-    var units = '<?=$meter->unit?>';
+    var title = '<?=html_entity_decode($meter->name)?>';
+    var units = '<?=html_entity_decode($meter->unit)?>';
 </script>
 <script src="highstock.js"></script>
 <div style="width: 100%; height: 500px;" id="chart"></div>
