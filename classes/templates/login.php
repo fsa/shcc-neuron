c4633af45c576ee8639810e623870cc9795b45d7
Sergey Fedotov <fsa@tavda.info>
2019-08-08 00:45:24 +0700

    Правки окна входа в систему.

--- classes/templates/login.php
+++ classes/templates/login.php
@@ -4,40 +4,44 @@ namespace Templates;
 
 class Login {
 
+    public $title='Вход на сайт';
+    public $url='./';
+    public $redirect_uri='';
     public $header;
 
     public function show() {
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title><?=$this->title?></title>
-        <link rel="stylesheet" type="text/css" href="/styles.css">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+<meta charset="utf-8">
+<title><?=$this->title?></title>
+<link rel="stylesheet" type="text/css" href="/styles.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <?=$this->header?>
-    </head>
-    <body bgcolor="white">
-        <div class="container">
-            <div class="card">
-            <div class="card-header bg-info">Вход</div>
-            <div class="card-body">
-            <form method="POST" action="./">
-                <div class="form-group">
-                    <label for="logn">Имя пользователя</label>
-                    <input type="text" class="form-control" name="login" placeholder="Введите имя пользователя">
-                    <small id="loginHelp" class="form-text text-muted">Укажите имя пользователья для входа в систему.</small>
-                </div>
-                <div class="form-group">
-                    <label for="password">Пароль</label>
-                    <input type="password" class="form-control" name="password" placeholder="Введите пароль">
-                </div>
-                <button type="submit" class="btn btn-primary">Войти</button>
-            </form>
-            </div>
-            </div>
-        </div>
-    </body>
+</head>
+<body>
+<div class="container">
+<div class="card">
+<div class="card-header"><?=$this->title?></div>
+<div class="card-body">
+<form method="POST" action="<?=$this->url?>">
+<input type="hidden" name="redirect_uri" value="<?=$this->redirect_uri?>">
+<div class="form-group">
+<label for="inputLogin">Логин</label>
+<input type="login" name="login" class="form-control" id="inputLogin" aria-describedby="loginHelp" placeholder="Введите имя пользователя">
+<!--small id="loginHelp" class="form-text text-muted">We'll never share your email with anyone else.</small-->
+</div>
+<div class="form-group">
+<label for="inputPassword">Пароль</label>
+<input type="password" name="password" class="form-control" id="inputPassword" placeholder="Введите пароль">
+</div>
+<button type="submit" class="btn btn-primary">Войти</button>
+</form>
+</div>
+</div>
+</div>
+</body>
 </html>
 <?php
     }
