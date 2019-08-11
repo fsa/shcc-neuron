3dc198abd1b8c080f2ad87372a29237b362bb55e
Sergey Fedotov <fsa@tavda.info>
2019-08-12 01:56:42 +0700

    Исправлен вход в систему.

--- classes/html.php
+++ classes/html.php
@@ -3,7 +3,11 @@
 class HTML {
 
     private static $_instance=null;
-    private static $header_sent=false;
+    private static $keywords=[];
+    private static $timestamp=null;
+    private static $title;
+    private static $header_shown=false;
+    private static $root_dir='./';
 
     private function __clone() {
         
@@ -15,38 +19,70 @@ class HTML {
 
     public static function getInstance() {
         if (self::$_instance===null) {
-            self::$_instance=new \Templates\HTML();
-            self::$_instance->header='';
+            self::$_instance=new Templates\HTML();
+            #TODO: вычислить каталог
         }
         return self::$_instance;
     }
 
-    public static function setTemplate($template) {
-        self::$_instance=new $template;
+    public static function setTemplate($name) {
+        $name='Templates\\'.$name;
+        self::$_instance=new $name();
+    }
+
+    public static function setVar($var, $value) {
+        $html=self::getInstance();
+        $html->$var=$value;
+    }
+
+    public static function getRootDir() {
+        return self::$root_dir;
+    }
+
+    public static function setRootDir($dir) {
+        self::$root_dir=$dir;
     }
 
     public static function getTitle() {
-        $html=self::getInstance();
-        return $html->title;
+        return self::$title;
     }
 
-    public static function showPageHeader($title=null,$timestamp=false) {
+    public static function showPageHeader($title=null) {
         $html=self::getInstance();
-        if (!$timestamp) {
-            $timestamp=filemtime(filter_input(INPUT_SERVER,'SCRIPT_FILENAME'));
+        self::$header_shown=true;
+        #$html->username=\User\Auth::getName();
+        if (!self::$timestamp) {
+            self::$timestamp=filemtime(getenv('SCRIPT_FILENAME'));
         }
         header("HTTP/1.0 200 OK");
         header("Content-Type: text/html; charset=utf-8");
-        header("Last-Modified: ".gmdate("D, d M Y H:i:s \G\M\T",$timestamp));
+        header("Last-Modified: ".gmdate("D, d M Y H:i:s \G\M\T", self::$timestamp));
         self::disableBrowserCache();
-        $html->title=$title;
+        $html->title=($title?$title.' :: ':'').\Settings::get('site')->title;
+        self::$title=$title?$title:\Settings::get('site')->title;
         $html->Header();
-        self::$header_sent=true;
+    }
+
+    public static function showTitleH1() {
+        $html=self::getInstance();
+        $html->title=self::$title;
+        $html->TitleH1();
     }
 
     public static function showPageFooter() {
         $html=self::getInstance();
         $html->Footer();
+    }
+
+    protected function showMetaKeywords() {
+        if (sizeof($this->keywords)==0) {
+            return;
+        }
+        echo '<meta name="keywords" content="'.join(',', $this->keywords).'">'.PHP_EOL;
+    }
+
+    public static function setLatModified($timestamp) {
+        self::$timestamp=strtotime($timestamp);
     }
 
     public static function addHeader($data) {
@@ -58,33 +94,39 @@ class HTML {
         self::addHeader("<meta name=\"description\" content=\"$description\">");
     }
 
-    public static function addKeyword($keywords) {
-        self::addHeader("<meta name=\"keywords\" content=\"$keywords\">");
+    public static function addLeafletJs() {
+        self::addHeader('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
+        self::addHeader('<link rel="stylesheet" href="/libs/leaflet/leaflet.css">');
+    }
+
+    public static function addKeyword($keyword) {
+        self::$keywords[]=$keyword;
     }
 
     public static function showException($message) {
-        $title='Ошибка';
+        $title='Внимание!';
         $message="<p>$message</p>".PHP_EOL;
-        if (self::$header_sent) {
-            self::showPopup($title,$message);
+        if (self::$header_shown) {
+            self::showPopup($title, $message);
             self::showPageFooter();
             return;
         }
         $html=new \Templates\Message();
         $html->style='danger';
         $html->title=$title;
+        $html->site_title=\Settings::get('site')->title;
         $html->message=$message;
         $html->show();
     }
 
-    public static function showPopup($title,$message) {
+    public static function showPopup($title, $message) {
         $html=new \Templates\Popup();
         $html->title=$title;
         $html->message="<h1>$title</h1>".PHP_EOL."$message";
         $html->show();
     }
 
-    public static function showNotification($title,$message,$url=null) {
+    public static function showNotification($title, $message, $url=null) {
         $html=new \Templates\Message();
         $message="<p>$message</p>".PHP_EOL;
         if (!is_null($url)) {
@@ -95,6 +137,7 @@ class HTML {
         self::disableBrowserCache();
         $html->title=$title;
         $html->message=$message;
+        $html->site_title=\Settings::get('site')->title;
         $html->show();
         die;
     }
@@ -109,38 +152,61 @@ class HTML {
         return getenv('HTTPS')!==false;
     }
 
-    public static function redirect($location,$response_code=303) {
-        header("Location: $location",true,$response_code);
+    public static function redirect($location) {
+        header("Location: $location");
         echo "<a href=\"$location\">$location</a>";
         exit;
     }
 
+    public static function redirect301($location) {
+        header(getenv('SERVER_PROTOCOL')." 301 Moved Permanently", true);
+        header("Location: $location");
+        exit;
+    }
+
     public static function error404() {
-        header(filter_input(INPUT_SERVER,'SERVER_PROTOCOL')." 404 Not Found",true,404);
+        header(getenv('SERVER_PROTOCOL')." 404 Not Found", true);
         echo 'File not found';
         exit;
     }
 
-    public static function showLoginForm($url=false,$redirect_url=false,$title=false) {
+    public static function showLoginForm($url=false) {
         $html=new \Templates\Login();
-        $html->title=$title?$title:'Вход в систему';
-        $html->url=$url?$url:'/login/';
-        $html->redirect_url=$redirect_url?$redirect_url:'/';
+        $html->title='Вход в систему';
+        $html->root_dir=self::$root_dir;
+        if ($url) {
+            $html->redirect_uri=$url;
+            $html->url='/login/';
+        }
         Header("Content-Type: text/html; charset=utf-8");
         self::disableBrowserCache();
         $html->show();
+        unset($html);
     }
-    
+
     public static function Exception($ex) {
-        if (method_exists($ex, 'Handler')) {
-            $ex->Handler($ex);
-            exit;
-        }
-        $message=$ex;
-        if (ini_get('display_errors')!=1) {
-            error_log($ex,0);
-            $message='Извините. Произошла программная ошибка. Если вы часто видите это сообщение, сообщите о нём администратору сайта.';
+        \DB::rollback();
+        switch (get_class($ex)) {
+            case 'AppException':
+                $message=$ex->getMessage();
+                break;
+            default:
+                $message=$ex;
+                if (\Settings::get('debug')!=true) {
+                    error_log($message, 0);
+                    $message='Извините. Произошла программная ошибка. Если вы часто видите это сообщение, сообщите о нём администратору сайта.';
+                }
         }
         self::showException($message);
     }
+
+    public static function __callStatic($name, $args) {
+        if (substr($name, 0, 4)!='show') {
+            throw new Exception('Вызван несуществующий метод '.$name);
+        }
+        $name=substr($name, 4);
+        $callback=array(self::getInstance(), $name);
+        return call_user_func_array($callback, $args);
+    }
+
 }
--- htdocs/login/index.php
+++ htdocs/login/index.php
@@ -4,9 +4,10 @@ require_once '../common.php';
 $login=filter_input(INPUT_POST,'login');
 $password=filter_input(INPUT_POST,'password');
 if (!$login or!$password) {
-    HTML::showLoginForm();
+    HTML::showLoginForm('/');
     exit;
 }
 $user=Auth\User::authenticate($login,$password);
 Auth\Internal::login($user);
-HTML::redirect('../');
+$url=filter_input(INPUT_POST,'redirect_uri');
+HTML::redirect(is_null($url)?'/':$url);
