89a83b1a1ecb145136651c8d5f078c7cbf79990c
Sergey Fedotov <fsa@tavda.info>
2019-08-26 02:05:19 +0700

    Исправлено создание элементов в древовидной структуре мест расположения устройств для корневого элемнента.

--- htdocs/api/places/create/index.php
+++ htdocs/api/places/create/index.php
@@ -3,8 +3,11 @@
 require_once '../../../common.php';
 Auth\Internal::grantAccess(['admin']);
 $text=filter_input(INPUT_GET, 'text');
-$parent=filter_input(INPUT_GET, 'parent');
-if(!$text or !$parent) {
+$parent=filter_input(INPUT_GET, 'parent', FILTER_VALIDATE_INT);
+if($parent===false) {
+    $parent=null;
+}
+if(!$text) {
     httpResponse::json(['error'=>'Неверные параметры запроса']);
 }
 if($text=='') {
