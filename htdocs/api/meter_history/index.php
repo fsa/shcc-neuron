4cfe7cdec09668d18dc1a1eb1ea6c94846024226
Sergey Fedotov <fsa@tavda.info>
2019-08-26 16:01:19 +0700

    Перехват ошибок при выдаче истории измерений и корректный ответ JSON.

--- htdocs/api/meter_history/index.php
+++ htdocs/api/meter_history/index.php
@@ -7,11 +7,16 @@ $meter_id=filter_input(INPUT_GET,'meter',FILTER_VALIDATE_INT);
 $meter_unit_id=filter_input(INPUT_GET,'unit',FILTER_VALIDATE_INT);
 $from=filter_input(INPUT_GET,'from');
 $to=filter_input(INPUT_GET,'to');
-$history=new SmartHome\MeterHistory();
-$history->setPlaceId($place_id,$meter_unit_id);
-$history->setMeterId($meter_id);
-$history->setFromDateTime($from);
-$history->setToDateTime($to);
-#TODO фильтрация по периоду
+try {
+    $history=new SmartHome\MeterHistory();
+    $history->setPlaceId($place_id,$meter_unit_id);
+    $history->setMeterId($meter_id);
+    $history->setFromDateTime($from);
+    $history->setToDateTime($to);    
+} catch (AppException $ex) {
+    httpResponse::json(['error'=>$ex->getMessage()]);
+} catch (Exception $ex) {
+    httpResponse::json(['error'=>'Internal error']);
+}
 header('Content-Type: application/json');
 echo $history->getJson();
