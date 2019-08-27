c9ca10a1f039f863ca046c8c2d99644c255c42b2
Sergey Fedotov <fsa@tavda.info>
2019-08-28 00:02:20 +0700

    Черновой вариант передачи команд управления устройствами.

---
+++ htdocs/api/command/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once '../../common.php';
+Auth\Internal::grantAccess(['control']);
+$dir='../../../custom/command/';
+$device_name=filter_input(INPUT_GET,'device_name');
+$filename=$device_name.'.php';
+if($device_name and file_exists($dir.$filename)) {
+    chdir($dir);
+    $result=require_once $filename;
+    httpResponse::json(['power'=>$result]);
+}
+httpResponse::json(['error'=>'Устройство не настроено']);
