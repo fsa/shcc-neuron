ed12dc0df746dd9028f436826959d250b5dcb781
Sergey Fedotov <fsa@tavda.info>
2019-08-30 01:48:31 +0700

    Проверка возможности управление лампой через Алису. Хардкод.

--- htdocs/alice-webhook/_v1.0_user_devices_action.php
+++ htdocs/alice-webhook/_v1.0_user_devices_action.php
@@ -4,10 +4,14 @@ if(!isset($request_id)) {die;}
 #\Auth\Bearer::grantAccess();
 #TODO: разгрести $request_content со списком устройств
 $request=json_decode($request_content);
+file_put_contents('action_'.date('Y_m_d').'.txt', print_r($request, true).PHP_EOL, FILE_APPEND | LOCK_EX);
 $id=0;
 foreach ($request->payload->devices as $device) {
+    $yeelight=\SmartHome\Devices::get($device->id);
+    $power=$device->capabilities[0]->state->value;
+    $yeelight->setPower($power);
     $devices[$id]=new \Yandex\SmartHome\DeviceState($device->id);
-    $state=new Yandex\SmartHome\Capabilities\onOffState('on', true);
+    $state=new Yandex\SmartHome\Capabilities\onOffState('on', $power);
     $state->setActionResultDone();
     $devices[$id]->addCapabilitie($state);
     $id++;
--- htdocs/alice-webhook/_v1.0_user_devices_query.php
+++ htdocs/alice-webhook/_v1.0_user_devices_query.php
@@ -3,13 +3,23 @@ if(!isset($request_id)) {die;}
 #\Auth\Bearer::grantAccess();
 #TODO: разгрести $request_content со списком устройств
 $request=json_decode($request_content);
+file_put_contents('query_'.date('Y_m_d').'.txt', print_r($request, true).PHP_EOL, FILE_APPEND | LOCK_EX);
 $id=0;
 foreach ($request->devices as $device) {
+    /* @var $yeelight Yeelight\GenericDevice */
+    $yeelight=\SmartHome\Devices::get($device->id);
+    $yeelight->refreshState();
+    $power=$yeelight->getPowerValue();
     $devices[$id]=new \Yandex\SmartHome\DeviceState($device->id);
-    $devices[$id]->addCapabilitie(new Yandex\SmartHome\Capabilities\onOffState('on', true));
+    $devices[$id]->addCapabilitie(new Yandex\SmartHome\Capabilities\onOffState('on', $power));
     $id++;
 }
-
+file_put_contents('query_'.date('Y_m_d').'.txt', print_r(json_encode([
+    'request_id'=>$request_id,
+    'payload'=>[
+        'devices'=>$devices
+    ]
+]), true).PHP_EOL, FILE_APPEND | LOCK_EX);
 httpResponse::json([
     'request_id'=>$request_id,
     'payload'=>[
