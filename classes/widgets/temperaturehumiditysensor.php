80382d200be56304b5dc58b5bca0a999652707dc
Sergey Fedotov <fsa@tavda.info>
2020-02-09 01:53:07 +0700

    Убран пробел между значениями температуры и знаком градуса в виджете.

--- classes/widgets/temperaturehumiditysensor.php
+++ classes/widgets/temperaturehumiditysensor.php
@@ -9,7 +9,7 @@ class TemperatureHumiditySensor {
 
     public static function show($sensor_name) {
         $sensor=Devices::get($sensor_name);
-        HTML::showCard('Кухня', '<span style="font-size: 3rem;">'.round($sensor->getTemperature(), 1).' &deg;C, '.round($sensor->getHumidity()).'%', date('d.m.Y H:i:s', $sensor->getLastUpdate()).'</span>');
+        HTML::showCard('Кухня', '<span style="font-size: 3rem;">'.round($sensor->getTemperature(), 1).'&deg;C, '.round($sensor->getHumidity()).'%', date('d.m.Y H:i:s', $sensor->getLastUpdate()).'</span>');
     }
 
 }
