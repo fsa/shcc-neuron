39a4c72ea339d44bca1502e68c0361b393ec093c
Sergey Fedotov <fsa@tavda.info>
2020-02-12 17:02:08 +0700

    Корректный перенос строки в виджете погоды.

--- classes/widgets/weather.php
+++ classes/widgets/weather.php
@@ -7,7 +7,7 @@ use HTML;
 class Weather {
 
     public static function show($sensor) {
-        HTML::showCard($sensor->place_name, sprintf('<span style="font-size: 2rem;">%d &deg;C (%d&deg;C), %d%%, %d м/с (%s)</span>', $sensor->getTemperature(), $sensor->getTempFeelsLike(), $sensor->getHumidity(), $sensor->getWindSpeed(), $sensor->getWindDirection()), date('d.m.Y H:i:s', $sensor->getLastUpdate()));
+        HTML::showCard($sensor->place_name, sprintf('<span style="font-size: 2rem;"><span class="text-nowrap">%d (%d)&deg;C</span>, %d%%, <span class="text-nowrap">%d м/с (%s)</span></span>', $sensor->getTemperature(), $sensor->getTempFeelsLike(), $sensor->getHumidity(), $sensor->getWindSpeed(), $sensor->getWindDirection()), date('d.m.Y H:i:s', $sensor->getLastUpdate()));
     }
 
 }
