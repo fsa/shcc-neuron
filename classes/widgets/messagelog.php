e111df3f76e10bc14a5f85f38ec8a3e4fac0b39b
Sergey Fedotov <fsa@tavda.info>
2020-03-11 17:01:21 +0700

    Лог голосовых сообщений перенесён в оперативную память.

--- classes/widgets/messagelog.php
+++ classes/widgets/messagelog.php
@@ -8,11 +8,7 @@ class MessageLog {
 
     public static function show() {
         $log=\Tts\Log::getLastMessages();
-        $log_message=[];
-        foreach ($log AS $row) {
-            $log_message[]=sprintf('%s %s', date('H:i:s', strtotime($row->timestamp)), $row->text);
-        }
-        HTML::showCard('Последние голосовые сообщения', join('<br>', $log_message));
+        HTML::showCard('Последние голосовые сообщения', join('<br>', $log));
     }
 
 }
