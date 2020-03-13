ba175b7d03eebc6a5d2fa35a6f177aff7e4caadf
Sergey Fedotov <fsa@tavda.info>
2020-03-13 14:55:07 +0700

    Тема страницы для заголовков в браузерах на мобильных устройствах.

--- classes/templates/html.php
+++ classes/templates/html.php
@@ -16,7 +16,7 @@ class HTML {
 <meta charset="utf-8">
 <title><?=$this->title?></title>
 <meta name="viewport" content="width=device-width">
-<meta name="theme-color" content="#17a2b8">
+<meta name="theme-color" content="#527779">
 <link rel="stylesheet" href="/bootstrap.css">
 <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 <script src="/libs/jquery/jquery.min.js"></script>
