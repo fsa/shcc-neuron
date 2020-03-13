7543c836197cdab6e626e42008f038ef08275c38
Sergey Fedotov <fsa@tavda.info>
2020-03-13 15:07:24 +0700

    Основной цвет темы добавлен для графиков.

--- htdocs/charts/highstock.js
+++ htdocs/charts/highstock.js
@@ -21,7 +21,8 @@ Highcharts.setOptions({
     },
     time: {
         timezoneOffset: - 420
-    }
+    },
+    colors: ['#527779', '#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572', '#FF9655', '#FFF263', '#6AF9C4']
 });
 
 function createChart(unit) {
