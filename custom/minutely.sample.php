633d480d52f14ab18ceff03a0a8e11c388674b61
Sergey Fedotov <fsa@tavda.info>
2020-03-26 22:49:16 +0700

    Новый файл примера ежеминутного скрипта. Убрано прямое обращение к OpenWeatherMap. Общая оптимизация.

--- custom/minutely.sample.php
+++ custom/minutely.sample.php
@@ -15,23 +15,30 @@ if ($time>='23:00' or $time<'10:00') {
         say('Ночной режим выключен');
     }
 }
-# Текущее время
-if ($minute==0) {
-    say('Новосибирское время '.$time);
-}
-# Получение текущего прогноза погоды и его озвучивание
-if ($minute==0) {
-    if (!$mute) {
-        $owm=new OpenWeatherMap\Api(\Settings::get('openweathermap')->api_key);
-        $owm->setCityId(\Settings::get('openweathermap')->city_id);
-        $weather=$owm->fetchCurrent();
-        if (!is_null($weather)) {
-            say('Температура воздуха '.Tts\Tools::ruDegC($weather->main->temp));
-        }
-    }
-}
-# Голосовое сообщение в определённое время (C.A.T.S.)
-if ($time=='18:55') {
-    say('Заканчивается чемпионат в котиках.');
+# Закат, восход и солнце в зените
+$sun=new SmartHome\SunInfo;
+$sun_info=$sun->getText($time);
+if($sun_info) {
+    say($sun_info);
 }
 
+# Текущее время
+if ($minute==0) {
+    say('Текущее время '.$time);
+}
+# Получение текущего прогноза погоды и озвучивание температуры
+# Требуется создание устройства openweathermap
+#if ($minute%30==0) {
+#    $owmd=getDevivce('openweathermap');
+#    $owmyes=$owmd->update();
+#}
+#if ($minute==0) {
+#    if ($owmyes) {
+#        say('Температура воздуха '.Tts\Tools::ruDegC($owmd->weather->main->temp));
+#    }
+#}
+
+# C.A.T.S.
+#if ($time=='18:55') {
+#    say('Заканчивается чемпионат в котиках');
+#}
