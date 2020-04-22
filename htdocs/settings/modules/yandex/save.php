6c8db5293cf7da876a882ee4812b2b18224cfcad
Sergey Fedotov <fsa@tavda.info>
2020-04-22 18:53:36 +0700

    Обновление документации.

--- htdocs/settings/modules/yandex/save.php
+++ htdocs/settings/modules/yandex/save.php
@@ -3,9 +3,9 @@ if(!isset($action)) {die;}
 $api_key=filter_input(INPUT_POST,'api_key');
 $voice=filter_input(INPUT_POST,'voice');
 $emotion=filter_input(INPUT_POST,'emotion');
-SmartHome\Vars::set('yandex.tts_key', $api_key);
-SmartHome\Vars::set('yandex.tts_voice', $voice);
-SmartHome\Vars::set('yandex.tts_emotion', $emotion);
+SmartHome\Vars::set('Yandex.TTS.Key', $api_key);
+SmartHome\Vars::set('Yandex.TTS.Voice', $voice);
+SmartHome\Vars::set('Yandex.TTS.Emotion', $emotion);
 $yandex=new Yandex\TtsApi($api_key);
 $yandex->setSpeaker($voice);
 $yandex->setEmotion($emotion);
