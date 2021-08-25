af23910bcde40e8f8cd291758afe420b8edbffa0
Sergey Fedotov <fsa@tavda.info>
2021-08-26 03:40:31 +0700

    Для демона ежеминутного таймера скрит выдаёт ответ не дожидаясь обработки.

--- webroot/api/timer/index.php
+++ webroot/api/timer/index.php
@@ -5,6 +5,7 @@ require_once '../../common.php';
 if (getenv('REMOTE_ADDR')!='127.0.0.1') {
     die('Wrong host');
 }
+fastcgi_finish_request();
 if (file_exists(CUSTOM_DIR.'minutely.php')) {
     chdir(CUSTOM_DIR);
     require 'minutely.php';
