024960b6df1acc02a3b51ec0fb0b95229b7bcdbb
Sergey Fedotov <fsa@tavda.info>
2021-12-10 03:36:37 +0700

    Не подключалась автозагрузка в пользовательских скритах.

--- custom/lib.php
+++ custom/lib.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
  * Не редактируйте данный файл. Он может быть изменён при обновлении системы.
  */
-if (!is_array(spl_autoload_functions())) {
-    set_include_path(get_include_path().':'.__DIR__.'/../webroot/classes/');
+if (!spl_autoload_functions()) {
+    set_include_path(get_include_path() . ':' . __DIR__ . '/../webroot/classes/');
     spl_autoload_extensions('.php');
     spl_autoload_register();
-    openlog("shcc@cli", LOG_PID|LOG_ODELAY, LOG_USER);
+    openlog("shcc@cli", LOG_PID | LOG_ODELAY, LOG_USER);
 }
 require_once 'functions.php';
