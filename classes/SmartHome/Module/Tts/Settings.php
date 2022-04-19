937af75517ff226163ef650aa657e1991c2abd99
Sergei Fedotov <fsa@tavda.info>
2022-04-20 01:00:16 +0700

    Заменён разделитель переменных с точки на двоеточие

--- classes/SmartHome/Module/Tts/Settings.php
+++ classes/SmartHome/Module/Tts/Settings.php
@@ -7,15 +7,15 @@ use SmartHome\Vars;
 class Settings {
 
     public static function setProvider(string $class_name, array $params) {
-        Vars::setJson('TTS.Provider', ['class'=>$class_name, 'properties'=>$params]);
+        Vars::setJson('TTS:Provider', ['class'=>$class_name, 'properties'=>$params]);
     }
 
     public static function getProvider() {
-        return Vars::getJson('TTS.Provider');
+        return Vars::getJson('TTS:Provider');
     }
 
     public static function dropProvider() {
-        Vars::drop('TTS.Provider');
+        Vars::drop('TTS:Provider');
     }
 
 }
--- webroot/settings/modules/yandex/save.php
+++ webroot/settings/modules/yandex/save.php
@@ -5,13 +5,13 @@ if (!isset($action)) {
 $key = filter_input(INPUT_POST, 'key');
 $speaker = filter_input(INPUT_POST, 'speaker');
 $emotion = filter_input(INPUT_POST, 'emotion');
-SmartHome\Vars::set('Yandex.TTS.Key', $key);
+SmartHome\Vars::set('Yandex:TTS:Key', $key);
 # Проверяем корректность настроек и сохраняем корректные
 $yandex = new Yandex\TtsApi(['key' => $key]);
 $yandex->setSpeaker($speaker);
-SmartHome\Vars::set('Yandex.TTS.Speaker', $speaker);
+SmartHome\Vars::set('Yandex:TTS:Speaker', $speaker);
 $yandex->setEmotion($emotion);
-SmartHome\Vars::set('Yandex.TTS.Emotion', $emotion);
+SmartHome\Vars::set('Yandex:TTS:Emotion', $emotion);
 SmartHome\Module\Tts\Settings::setProvider(Yandex\TtsApi::class, ['key' => $key, 'speaker' => $speaker, 'emotion' => $emotion]);
 App::response()->storeNotification('В качестве синтезатора речи установлен Yandex SpeechKit. Голос ' . $speaker . ', эмоциональная окраска голоса ' . $emotion . '. Для вступления изменений в силу необходимо перезапустить демона TTS.');
 App::response()->redirection('../');
