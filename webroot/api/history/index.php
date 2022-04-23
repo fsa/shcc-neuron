4c0fad6210b1dd0359f929399bb02ea282f6a5fb
Sergei Fedotov <fsa@tavda.info>
2022-04-23 21:49:42 +0700

    Наименование класса вынесено в use

--- webroot/api/history/index.php
+++ webroot/api/history/index.php
@@ -1,9 +1,11 @@
 <?php
 
+use SmartHome\Sensors;
+
 require_once '../../../vendor/autoload.php';
 App::initJson();
 App::session()->grantAccess();
 $uid=filter_input(INPUT_GET, 'uid');
 $from=filter_input(INPUT_GET, 'from');
 $to=filter_input(INPUT_GET, 'to');
-App::response()->jsonString(SmartHome\Sensors::getHistoryJson($uid, $from, $to));
+App::response()->jsonString(Sensors::getHistoryJson($uid, $from, $to));
