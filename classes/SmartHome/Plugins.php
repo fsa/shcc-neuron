d35b949aa097b69ea103be44c0336d9c34710934
Sergei Fedotov <fsa@tavda.info>
2022-04-26 23:50:10 +0700

    Изменение работы с плагинами. Добавлена возможность получить список плагинов.

--- classes/SmartHome/Plugins.php
+++ classes/SmartHome/Plugins.php
@@ -2,10 +2,15 @@
 
 namespace SmartHome;
 
+use Composer\Autoload\ClassLoader;
 use Composer\Installer\PackageEvent;
 
 class Plugins
 {
+    const CONFIG_PATH = '/../shcc-plugins.json';
+    private static $plugins;
+    private static $vendor_dir;
+
     public static function prePackageInstall(PackageEvent $event)
     {
         $plugin = self::searchPlugin($event);
@@ -34,31 +39,49 @@ class Plugins
         if (!(isset($plugin['name']) and isset($plugin['namespace']))) {
             return null;
         }
-        $plugin['config'] = $event->getComposer()->getConfig()->get('vendor-dir') . '/../shcc-plugins.json';
+        self::$vendor_dir = $event->getComposer()->getConfig()->get('vendor-dir');
         return $plugin;
     }
 
     private static function addPlugin($data)
     {
-        $config = file_exists($data['config']) ? json_decode(file_get_contents($data['config']), true, 512, JSON_THROW_ON_ERROR) : [];
-        if (!is_array($config)) {
-            $config = [];
-        }
-        $config[$data['name']] = $data['namespace'];
-        file_put_contents($data['config'], json_encode($config));
+        self::get();
+        self::$plugins[$data['name']] = $data['namespace'];
+        self::set();
     }
 
     private static function removePlugin($data)
     {
-        if (!file_exists($data['config'])) {
-            echo 'Файл со списком плагинов не существует' . PHP_EOL;
-            return;
-        }
-        $config = json_decode(file_get_contents($data['config']), true, 512, JSON_THROW_ON_ERROR);
-        $key = array_search($data['namespace'], $config);
+        self::get();
+        $key = array_search($data['namespace'], self::$plugins);
         if ($key !== false) {
-            unset($config[$key]);
+            unset(self::$plugins[$key]);
         }
-        file_put_contents($data['config'], json_encode($config));
+        self::set();
+    }
+
+    public static function get()
+    {
+        if (is_null(self::$plugins)) {
+            if (is_null(self::$vendor_dir)) {
+                $reflection = new \ReflectionClass(ClassLoader::class);
+                self::$vendor_dir = dirname($reflection->getFileName(), 2);
+            }
+            $config_file = self::$vendor_dir . self::CONFIG_PATH;
+            self::$plugins = file_exists($config_file) ? json_decode(file_get_contents($config_file), true, 512, JSON_THROW_ON_ERROR) : [];
+            if (!is_array(self::$plugins)) {
+                throw new \Exception('Plugins config file format error');
+            }
+        }
+        return self::$plugins;
+    }
+
+    private static function set()
+    {
+        if (is_null(self::$vendor_dir)) {
+            throw new \Exception('Get config first');
+        }
+        $config_file = self::$vendor_dir . self::CONFIG_PATH;
+        file_put_contents($config_file, json_encode(self::$plugins));
     }
 }
