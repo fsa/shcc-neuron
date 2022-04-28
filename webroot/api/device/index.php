147ae4326d8c2ea7369312c4a571fb23eabccd70
Sergei Fedotov <fsa@tavda.info>
2022-04-28 22:47:30 +0700

    Исправлено управление устройствами.

--- webroot/api/device/index.php
+++ webroot/api/device/index.php
@@ -1,25 +1,27 @@
 <?php
 
+use FSA\SmartHome\Capability\{PowerInterface, BrightnessInterface, ColorTInterface};
+
 require_once '../../../vendor/autoload.php';
-App::initJson();
+$response = App::initJson();
 App::session()->grantAccess(['control']);
 $device_name = filter_input(INPUT_GET, 'name');
 if (!$device_name) {
-    App::response()->returnError(400);
+    $response->returnError(400);
 }
 if (file_exists($device_name . '.php')) {
     chdir('../../../custom/command/');
     require_once '../functions.php';
     try {
         $result = require_once $device_name . '.php';
-        App::response()->json($result);
+        $response->json($result);
     } catch (AppException $ex) {
-        App::response()->returnError(500, $ex->getMessage());
+        $response->returnError(500, $ex->getMessage());
     }
 }
-$device = SmartHome\Devices::get($device_name);
+$device = SmartHome::getDevice($device_name);
 if (is_null($device)) {
-    App::response()->returnError(400);
+    $response->returnError(400);
 }
 $request = json_decode(file_get_contents('php://input'));
 if ($request) {
@@ -27,17 +29,23 @@ if ($request) {
         $value = $request->value;
         switch ($request->action) {
             case 'power':
-                $device->setPower($value);
+                if ($device instanceof PowerInterface) {
+                    $device->setPower($value);
+                }
                 break;
             case 'bright':
-                $device->setBrightness($value);
+                if ($device instanceof BrightnessInterface) {
+                    $device->setBrightness($value);
+                }
                 break;
             case 'ct':
-                $device->setCT($value);
+                if ($device instanceof ColorTInterface) {
+                    $device->setCT($value);
+                }
                 break;
         }
     } catch (AppException $ex) {
-        App::response()->returnError(500, $ex->getMessage());
+        $response->returnError(500, $ex->getMessage());
     }
 }
-App::response()->json(['properties' => $device->getState(), 'last_update' => $device->getLastUpdate()]);
+$response->json(['properties' => $device->getState(), 'last_update' => $device->getLastUpdate()]);
