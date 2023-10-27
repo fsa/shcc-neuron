3508b0573f41186ccddfe6cbbe945cc79b3d3075
Sergei Fedotov <fsa@tavda.info>
2023-10-27 18:23:49 +0700

    SMSD

--- webroot/api/smsd/index.php
+++ webroot/api/smsd/index.php
@@ -4,23 +4,16 @@ if (getenv('REMOTE_ADDR')!='127.0.0.1') {
     die('Wrong host');
 }
 #fastcgi_finish_request();
-require_once '../../common.php';
+require_once '../../../custom/lib.php';
 $id=filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
 if(!$id) {
     die('ID!');
 }
-$telegram=Settings::get('telegram');
-if(!$telegram) {
-    die('TG config!');
-}
-Telegram\Query::init($telegram);
-$s=DB::prepare('SELECT "TextDecoded" FROM inbox WHERE "ID"=?');
+$s=App::sql()->prepare('SELECT "SenderNumber" as num, "TextDecoded" as text FROM inbox WHERE "ID"=?');
 $s->execute([$id]);
-$text=$s->fetchColumn();
-if(!$text) {
-    die('Text');
+$msg=$s->fetchObject();
+#var_dump($msg);
+if(!$msg or !($msg->text)) {
+    die('No Message!');
 }
-$api=new Telegram\SendMessage($telegram['log_channel'], "Вам пришла SMS:\n".$text);
-#$api->setParseModeMarkdown();
-$api->setDisableWebPagePreview();
-$api->httpPostJson();
+telegram("Пришло SMS от ".$msg->num.":\n".$msg->text);
